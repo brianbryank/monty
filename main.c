87f9b6a83bc7be1479cfccec5a6f214074878106
brianbryank <brianbryan0125@gmail.com>
2021-12-03 20:14:53 +0300

    update

--- main.c
+++ main.c
@@ -1,47 +1,19 @@
 #include "monty.h"
-
-vars var;
+stack_t *head = NULL;
 
 /**
- * main - Start LIFO, FILO program
- * @ac: Number of arguments
- * @av: Pointer containing arguments
- * Return: 0 Success, 1 Failed
+ * main - Entry to the program
+ * @argc: Number of command line arguments.
+ * @argv: An array containing the arguments.
+ * Return: Always Zero.
  */
-int main(int ac, char **av)
+int main(int argc, char **argv)
 {
-	char *opcode;
-
-	if (ac != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		return (EXIT_FAILURE);
-	}
-
-	if (start_vars(&var) != 0)
-		return (EXIT_FAILURE);
-
-	var.file = fopen(av[1], "r");
-	if (!var.file)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", av[1]);
-		free_all();
-		return (EXIT_FAILURE);
-	}
-
-	while (getline(&var.buff, &var.tmp, var.file) != EOF)
-	{
-		opcode = strtok(var.buff, " \r\t\n");
-		if (opcode != NULL)
-			if (call_funct(&var, opcode) == EXIT_FAILURE)
-			{
-				free_all();
-				return (EXIT_FAILURE);
-			}
-		var.line_number++;
-	}
-
-	free_all();
-
-	return (EXIT_SUCCESS);
+	/*argc must be 2*/
+	if (argc < 2 || argc > 2)
+		error_out(1);
+	open_file(argv[1]);
+	/*Free memory*/
+	free_nodes();
+	return (0);
 }
